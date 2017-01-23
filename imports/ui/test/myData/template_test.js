070f9d38f148f1c974877a69b2ed6752466352fe
cybuzz <neeraj@cybuzzsc.com>
2017-01-23 21:42:46 +0530

    using let

--- imports/ui/test/myData/template_test.js
+++ imports/ui/test/myData/template_test.js
@@ -1,32 +1,71 @@
-import {Meteor} from 'meteor/meteor';
+import {
+    Meteor
+} from 'meteor/meteor';
 import './template_test.html';
-import {Template} from 'meteor/templating';
-import {ReactiveVar} from 'meteor/reactive-var';
+import {
+    Template
+} from 'meteor/templating';
+import {
+    ReactiveVar
+} from 'meteor/reactive-var';
 
-Template.templateTest.onCreated (function(){
+Template.templateTest.onCreated(function() {
 
 });
 
-Template.templateTest.rendered = function(){
+Template.templateTest.rendered = function() {
 
 };
 
 Template.templateTest.events({
-  'click #viewReportDateWise'(e, instance){
-    var data = $('#txtData').val();
-    Meteor.call("checkPasswordToRefresh",data, function(error, result) {
-        if (error) {
-            alert("Please try again!");
-        } else {
-            if (result.status) {
-              alert("AAAAAA");
+    'click #viewReportDateWise' (e, instance) {
+        var data = $('#txtData').val();
+        Meteor.call("checkPasswordToRefresh", data, function(error, result) {
+            if (error) {
+                alert("Please try again!");
+            } else {
+                if (result.status) {
+                    alert("AAAAAA");
+                }
             }
-        }
-    });
-    console.log(data);
-  }
+        });
+        console.log(data);
+    }
 });
 
 Template.templateTest.helpers({
-
+    aarHelper() {
+        var data = [
+            [1, 2, 3, 4, 5, 15],
+            [1, 2, 3, 4, 5, 15],
+            [1, 2, 3, 4, 5, 15],
+            [1, 2, 3, 4, 5, 15],
+            [4, 8, 12, 16, 20, 60]
+        ];
+        var dataloss = [
+            [6, 7, 8, 9, 10, 40],
+            [6, 7, 8, 9, 10, 40],
+            [6, 7, 8, 9, 10, 40],
+            [6, 7, 8, 9, 10, 40],
+            [24, 28, 32, 36, 40, 160]
+        ];
+        var json = {
+            data: data,
+            dataloss: dataloss
+        };
+        return json;
+    },
+    loooping() {
+        var ary = [];
+        for (var i = 0; i < 6; i++) {
+            ary.push(i)
+        }
+        return ary;
+    },
+    helper(ary, index, val) {
+      console.log(ary);
+      console.log(index);
+      console.log(val);
+        return ary[index][val];
+    }
 });
